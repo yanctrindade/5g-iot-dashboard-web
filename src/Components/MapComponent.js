7bca69c2eb323af49f6f100eaa965dde5286cc72
lucascamposj <lucascj29@gmail.com>
2019-10-22 11:14:37 -0300

    Initial Google Maps implementation

--- src/Components/MapComponent.js
+++ src/Components/MapComponent.js
@@ -1,68 +1,31 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-
-import Map from 'ol/Map';
-import View from 'ol/View';
-import TileLayer from 'ol/layer/Tile';
-//import OSM from 'ol/source/OSM';
-import XYZ from 'ol/source/XYZ';
-import MapCard from './MapCard';
-import "ol/ol.css";
-import {ZoomSlider} from 'ol/control';
-import {fromLonLat} from 'ol/proj';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
+import MapCard from './MapCard'
 
 class MapComponent extends Component {
 
-  constructor(props) {
-    super(props);
-    this.mapRef = null;
-    this.map = null;
-    this.layer = null;
-    this.view = null;
-    this.setMapRef = element => {
-      this.mapRef = element;
-    }
-  }
-
   render() {
     return (
-      <div className="map" id="map" ref={this.setMapRef}>
-        <MapCard />
-      </div>
-    )
+      <>
+        <Map
+          google={this.props.google}
+          zoom={16}
+          style={mapStyles}
+          initialCenter={{ lat: -15.762919, lng:-47.869899}}
+        />
+        <MapCard/>
+      </>
+    );
   }
 
-  componentDidMount() {
-    const mapDOMNode = ReactDOM.findDOMNode(this.mapRef);
-    this.layer = new TileLayer({
-      preload: Infinity,
-      source: new XYZ({
-        url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-      })
-    });
-    this.view = new View({
-      center: fromLonLat([-47.869899,-15.762919]),
-      zoom: 17
-    });
-    this.map = new Map({
-      target: mapDOMNode,
-      layers: [
-        this.layer
-      ],
-      view: this.view
-    });
-    let zoomslider = new ZoomSlider();
-    this.map.addControl(zoomslider);
-  }
-
-  componentWillReceiveProps(nextProps) {
-    console.log("UpdateSize");
-  }
-
-  shouldComponentUpdate(newProps, newState) {
-    console.log("shouldComponentUpdate: " + !newProps.update);
-    return (!newProps.update);
-  }
 }
 
-export default MapComponent
+const mapStyles = {
+  width: '100%',
+  height: '100%',
+  position: 'relative'
+};
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyDHYWWkJ5p-Du3DKUuJgQoXUAcqyPmwjIQ'
+})(MapComponent);
