4537d5faa8616278cac46da18d57a9733cfb2c64
Andre Garrido <andre.garridod@gmail.com>
2019-11-08 10:35:06 -0300

    Added pie chart to statistics page

--- src/Components/Statistics.js
+++ src/Components/Statistics.js
@@ -1,9 +1,42 @@
 import React from "react";
-import StatisticsCard from "./StatisticsCard"
+import StatisticsCard from "./StatisticsCard";
+import {Pie} from "react-chartjs-2";
+
+const data = {
+	labels: [
+		'Quebrado',
+		'Operando',
+    'Em manuntenção',
+    'Sem status'
+	],
+	datasets: [{
+		data: [2, 10, 15, 5],
+		backgroundColor: [
+		'#FF0000',
+		'#008000',
+    '#FFFF00',
+    '#808080'
+		],
+		hoverBackgroundColor: [
+		'#FF0000',
+		'#008000',
+    '#FFFF00',
+    '#808080'
+		]
+	}]
+};
 
 function Statistics(props) {
+  
+
   return <div>
     <StatisticsCard />
+    <Pie data={data} options={{
+          responsive: true,
+          maintainAspectRatio: true,
+        }}
+        height='50%'
+        />
   </div>;
 }
 
