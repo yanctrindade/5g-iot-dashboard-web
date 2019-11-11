2f609966d9f3a09b44a748caa546de46ad5cda1b
lucascamposj <lucascj29@gmail.com>
2019-11-11 20:10:25 -0300

    add multiple markers

--- src/Components/MapComponent.js
+++ src/Components/MapComponent.js
@@ -1,9 +1,46 @@
 import React, { Component } from 'react';
-import { Map, Polyline, GoogleApiWrapper } from 'google-maps-react';
-import MapCard from './MapCard'
-import coordinates from '../FakeData/Coordinates.js'
+import { Map, Polyline, Marker, GoogleApiWrapper } from 'google-maps-react';
+import MapCard from './MapCard';
+import coordinates from '../FakeData/Coordinates.js';
+import axios from 'axios';
 
 class MapComponent extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {markers: []};
+  }
+
+  renderPaths = (coords) => (
+    <Polyline
+    path={coords}
+    strokeColor="#0000FF"
+    strokeOpacity={0.5}
+    strokeWeight={4} />
+  )
+  
+  addMarker = (marker) => {
+    const {key, plate, currentLocation} = marker;
+    console.log(`addMarker ${key} -> ${currentLocation.lat} ${currentLocation.lng}`);
+    return(
+      <Marker
+      key={key}
+      position={currentLocation}
+      name={plate}
+      />
+    )
+  }
+
+  componentDidMount() {
+
+    axios.get('./database.json')
+    .then((res)=>{
+      console.log(res.data);
+      this.setState({markers: res.data});
+    }).catch((err)=>{
+      console.log(err);
+    })
+  }
 
   render() {
 
@@ -17,11 +54,7 @@ class MapComponent extends Component {
           style={mapStyles}
           initialCenter={coords[Math.round(coords.length/2)]}
         >
-          <Polyline
-          path={coords}
-          strokeColor="#0000FF"
-          strokeOpacity={0.5}
-          strokeWeight={4} />
+        {this.state.markers.map( marker => this.addMarker(marker))}
         </Map>
         <MapCard/>
       </>
