e9f7845ceda1c08f124353031ea286518127587c
Andre Garrido <andre.garridod@gmail.com>
2020-01-08 16:04:24 -0300

    Minor refactoring

--- src/Components/Routes/NotFound.js
+++ src/Components/Routes/NotFound.js
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
-import { Card } from 'antd';
+import { Card, Button } from 'antd';
 import "./styles.css"
 
 class NotFound extends Component {
     render(){
         return (
             <Card title="Erro 404 - Página não encontrada." className={"not-found-card"}>
-                <p>Clique <a onClick={() => this.props.history.goBack()}>aqui</a> para voltar para a página anterior.</p>
+                <p>Clique <Button 
+                            type="link" 
+                            onClick={() => this.props.history.goBack()}
+                            shape={"circle"}
+                        >
+                            aqui
+                        </Button> para voltar para a página anterior.</p>
             </Card>
         );
     }
